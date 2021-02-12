2876cedde35e5a44568d14f4516c0f4cb9f108c3
Tarandeep Nandhra <76442987+taran314@users.noreply.github.com>
2021-02-12 10:57:56 +0000

    Created DogLauncher class which test drives Dog class

---
+++ lec2/DogLauncher.java
@@ -0,0 +1,12 @@
+/**
+The DogLauncher class will test drive the Dog class
+Java goes to Dog.java and finds the makeNoise method.
+*/
+
+public class DogLauncher {
+
+    public static void main(String[] args) {
+        Dog.makeNoise();
+    }
+
+}

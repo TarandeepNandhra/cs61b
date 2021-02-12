0fd2bd8f27ce9241038bb43299a6d0172bcdd7cd
Tarandeep Nandhra <76442987+taran314@users.noreply.github.com>
2021-02-12 11:07:32 +0000

    Creating an instanciation of the class dog

--- lec2/DogLauncher.java
+++ lec2/DogLauncher.java
@@ -6,7 +6,10 @@ Java goes to Dog.java and finds the makeNoise method.
 public class DogLauncher {
 
     public static void main(String[] args) {
-        Dog.makeNoise();
+        // create a new dog d
+        Dog d = new Dog();
+        d.weightInPounds = 51;
+        d.makeNoise();
     }
 
 }

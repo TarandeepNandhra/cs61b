c8e6238e5dbf15f777dde4d44d2deb15add17b21
Tarandeep Nandhra <76442987+taran314@users.noreply.github.com>
2021-02-12 10:53:44 +0000

    Dog class with a call to method

---
+++ lec2/Dog.java
@@ -0,0 +1,11 @@
+public class Dog {
+
+    public static void makeNoise() {
+        System.out.println("Bark!");
+    }
+
+    public static void main(String[] args) {
+        makeNoise();
+    }
+
+}

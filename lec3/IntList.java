81f829535d854991fdfcc2afb432238c9d05e752
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-02-18 10:15:41 +0000

    Added size method, which calculate the size of a linked list via recursion

--- lec3/IntList.java
+++ lec3/IntList.java
@@ -6,6 +6,17 @@ public class IntList {
         first = f;
         rest = r;
     }
+    /** Return the size of the list using... recusion! */
+    public int size() {
+        // keep going through list until rest is null
+        if (this.rest == null) {
+            return 1;
+        }
+        else {
+            return 1 + this.rest.size();
+        }
+    }
+
 
     // L = new IntList(10, L);
     // rest = r means that the rest of the new list instance points to L so it's not lost.
@@ -14,5 +25,7 @@ public class IntList {
         IntList L = new IntList(15, null);
         L = new IntList(10, L);
         L = new IntList(5, L);
+        L.size();
     }
+
 }

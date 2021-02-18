ffd7c5f5bc98989cabc399bd8c9dabe5ba87ad48
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-02-18 10:59:33 +0000

    Implemeted challenge question dincrList

--- lec3/IntList.java
+++ lec3/IntList.java
@@ -33,7 +33,7 @@ public class IntList {
 
     // can't do this = this.rest as this is a "final" variable
 
-    /** Return the ith item in the list */
+    /** Return the ith item in this IntList */
     public int get(int i) {
 
         if (i == 0) {
@@ -42,11 +42,36 @@ public class IntList {
         return this.rest.get(i - 1);
     }
 
+    // don't need the this, it's implicit.
+
+    /** Return an IntList identical to L, but with all values incremented by x. */
+    // public static IntList incrList(IntList L, int x) {
+    //     IntList Q;
+    //     Q.first = L.first + x;
+    //     while (L.rest != null) {
+    //         L = L.rest;
+    //
+    //     }
+    //
+    // }
+
+    /** Return an IntList identical to L, but with all values incremented by x. */
+    public static IntList dincrList(IntList L, int x) {
+        IntList M = L;
+        M.first += x;
+        while (M.rest != null) {
+            M = M.rest;
+            M.first += x;
+        }
+        return L;
+    }
+
     public static void main(String[] args) {
         IntList L = new IntList(15, null);
         L = new IntList(10, L);
         L = new IntList(5, L);
         System.out.println(L.get(1));
+        IntList Q = dincrList(L, 5);
     }
 
 }

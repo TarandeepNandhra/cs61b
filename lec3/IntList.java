051b614b163e60e6545bd517c5267c546d653149
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-02-18 10:00:17 +0000

    Building linked list backwards

--- lec3/IntList.java
+++ lec3/IntList.java
@@ -2,16 +2,17 @@ public class IntList {
     public int first;
     public IntList rest;
 
-    public static void main(String[] args) {
-        IntList L = new IntList();
-        L.first = 5;
-        L.rest = null;
-
-        L.rest = new IntList();
-        L.rest.first = 10;
-
-        L.rest.rest = new IntList();
-        L.rest.rest.first = 15;
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
     }
 
+    // L = new IntList(10, L);
+    // rest = r means that the rest of the new list instance points to L so it's not lost.
+
+    public static void main(String[] args) {
+        IntList L = new IntList(15, null);
+        L = new IntList(10, L);
+        L = new IntList(5, L);
+    }
 }

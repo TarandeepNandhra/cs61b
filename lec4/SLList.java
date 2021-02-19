c9128b77355405e8836c64c2a17db0d76f2e9ce4
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-02-19 10:09:00 +0000

    Improved functuality by caching the size of the list within SLList

--- lec4/SLList.java
+++ lec4/SLList.java
@@ -16,15 +16,18 @@ public class SLList {
     // first can only be used in the SLList class
     // can be used as part of an abstraction barrier, hides implementation.
     private IntNode first;
+    private int size;
 
     public SLList(int x) {
         first = new IntNode(x, null);
+        size = 1;
 
     }
 
     /** Adds x to the front of the list. */
     public void addFirst(int x) {
         first = new IntNode(x, first);
+        size++;
     }
 
     /** Returns the first item in the list. */
@@ -35,35 +38,28 @@ public class SLList {
     /** Adds an item to the end of the list. */
     public void addLast(int x) {
         IntNode p = first;
+        size++;
         while( p.next != null) {
             p = p.next;
         }
         p.next = new IntNode(x, null);
     }
 
+
     /** Returns the size of the list that starts at IntNode p. */
     // helper method for SLList size as SLList is not inheritly recursive.
-    private static int size(IntNode p) {
-        if (p.next == null) {
-            return 1;
-        }
-        return 1 + size(p.next);
-    }
-
-    public int size() {
-        return size(first);
-    }
-
-    // Below does not work as to work with IntNode means you must return an IntNode
-    // but size method below was invoked by an SLList.
-
-    // public int size() {
-    //     if (first.next == null) {
+    // private static int size(IntNode p) {
+    //     if (p.next == null) {
     //         return 1;
     //     }
-    //     first.next = first.next.next;
-    //     return 1 + first.size();
+    //     return 1 + size(p.next);
     // }
+
+    /** Returns the size of the list that starts at IntNode p. */
+    public int size() {
+        return size;
+    }
+
 
     public static void main(String[] args) {
         // List of one integer, without specifying null
